02b82c86d7b4516933047addbc82230d68db4342
ChaosGuru <chaos.feature@gmail.com>
2020-12-09 17:25:59 +0200

    compute ranks

--- js/repository.js
+++ js/repository.js
@@ -25,12 +25,41 @@ function getCoords(place, callback = loadWeather){
 
 
 function parseWeather(data) {
-    // var myjson = JSON.stringify(data, null, 2);
-    console.log(data["daily"][0]);
+    // var myjson = JSON.stringify(data["daily"], null, 2);
+    // console.log(myjson);
+    days = data["daily"]
+
+    let fomula = (feels_like, rain, snow, wind_speed, clouds) => (1000-((Math.abs(feels_like-22) + Math.sqrt(100*((rain + snow))/24) + Math.pow(wind_speed/12, 2))*10 + clouds));
+
+    // get ranks for current place
+    // weather = [];
+    ranks = [];
+    for (let day = 0; day < 7; day++) {
+        ranks[day] = fomula(
+            days[day]["feels_like"]["day"],
+            days[day]["rain"] ? days[day]["rain"] : 0,
+            days[day]["snow"] ? days[day]["snow"] : 0,
+            days[day]["wind_speed"],
+            days[day]["clouds"]);
+
+        // weather[day] = [
+        //     days[day]["feels_like"]["day"],
+        //     days[day]["rain"] ? days[day]["rain"] : 0,
+        //     days[day]["snow"] ? days[day]["snow"] : 0,
+        //     days[day]["wind_speed"],
+        //     days[day]["clouds"]
+        // ]
+    }
+
+    // console.log("feels_like | rain | snow | wind_speed | clouds");
+    // for (let day = 0; day < 7; day++ ) {
+    //     console.log(ranks[day], weather[day]);
+    // }
 
     // get already ranked places
 
-    // rank with new place
 
     // display in different order
+    
+
 }
