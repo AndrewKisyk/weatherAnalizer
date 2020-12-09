a88b9fa478f28d195d0920e5a637b8c1b9d647a3
ChaosGuru <chaos.feature@gmail.com>
2020-12-09 11:11:52 +0200

    get weather

--- js/repository.js
+++ js/repository.js
@@ -1,5 +1,11 @@
 //TODO реалізувати отримання погоди
 
-function loadWeather(){
-	
+function loadWeather(lat, lon){
+    let apiKey = "7925cd67e40e4e3dcdc04d6a4ce8d992";
+    let exlude = "current,minutely,hourly";
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exlude}&appid=${apiKey}&units=metric`;
+
+    console.log(data);
+    return data;
 }
+
